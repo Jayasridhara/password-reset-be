ce5f0e984342ccbed7762dce495561063df52d30
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-27 13:30:04 +0530

    update router

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: "gmail",
     port:465,
-    secure:false,
+    secure:true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
