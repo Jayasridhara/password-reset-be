72ae4db9c7733825462514dada77020ee61fa41d
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-27 13:41:54 +0530

    fast render

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: "gmail",
-    port:465,
-    secure:false,
+    port:587,
+    secure:true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
