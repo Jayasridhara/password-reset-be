3742a28d9836d195af59d4f927e33203cf510ccf
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-27 06:01:41 +0530

    lastrender

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,19 +1,22 @@
 const nodemailer = require('nodemailer');
-const sendEmail = async ({ email, subject, message }) => {
-  const transporter = nodemailer.createTransport({
-    service: 'SendGrid',
+const transporter = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
-  });
+});
 
-  await transporter.sendMail({
-    from: `"Password Reset" <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject,
-    html: message,
-  });
-};
+const sendEmail = async ({ email, subject, message }) => {
+    const mailOptions = {
+        from: process.env.EMAIL_PASS,
+        to: email,
+        subject,
+        html: message,
+    }
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+}
 
 module.exports = sendEmail;
