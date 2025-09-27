7007729eab43b6b99afe2bb7b17e5c9b1ae65d98
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-27 15:54:55 +0530

    lastrender

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,17 @@
 const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure:false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-        
-    },
-});
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+ const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "apikey", // literally "apikey"
+      pass: process.env.SENDGRID_API_KEY, // your SendGrid API key
+    },
+  });
 const sendEmail = async ({ email, subject, message }) => {
     const mailOptions = {
         from: process.env.EMAIL_USER, // must be the Gmail sender
@@ -21,5 +23,26 @@ const sendEmail = async ({ email, subject, message }) => {
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent: " + info.response);
 }
+
+
+//  const sendEmail = async ({ email, subject, message }) => {
+//     const msg = {
+//     to:email,
+//     from:process.env.EMAIL_USER, // Must be a verified sender in SendGrid
+//     subject,
+//     message
+//   };
+
+//   try {
+//     await sgMail.send(msg);
+   
+//   } catch (error) {
+//     console.error(error);
+//     if (error.response) {
+//       console.error(error.response.body);
+//     }
+    
+//   }
+//  }
 
 module.exports = sendEmail;
